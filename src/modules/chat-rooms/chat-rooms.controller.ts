19a434a61b49fc36dc2303db62c14329a0ee09fa
danrdzprz <daniel.rpda@gmail.com>
2024-11-06 22:21:24 -0600

    feat: update api docs

--- src/modules/chat-rooms/chat-rooms.controller.ts
+++ src/modules/chat-rooms/chat-rooms.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, Query, UseGuards } from '@nestjs/common';
 import { ChatRoomsService } from './chat-rooms.service';
 import { CreateChatRoomDto } from './dto/create-chat-room.dto';
 import { UpdateChatRoomDto } from './dto/update-chat-room.dto';
@@ -24,36 +24,42 @@ export class ChatRoomsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Regresa una lista de salas de chat paginadas.' })
   findAll(@Query() options:PaginationOptions) {
     return this.chatRoomsService.findAll(options);
   }
 
   @Get(':id')
   @UseGuards(IsValidChatRoom)
+  @ApiOperation({ summary: 'Regresa el detalle de la sala de chat.' })
   findOne(@Param('id') id: string) {
     return this.chatRoomsService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   @UseGuards(IsValidChatRoom)
+  @ApiOperation({ summary: 'Actualiza la sala de chat.' })
   update(@Param('id') id: string, @Body() updateChatRoomDto: UpdateChatRoomDto) {
     return this.chatRoomsService.update(id, updateChatRoomDto);
   }
 
   @Delete(':id')
   @UseGuards(IsValidChatRoom)
+  @ApiOperation({ summary: 'Elimina una sala de chat.' })
   remove(@Param('id') id: string) {
     return this.chatRoomsService.remove(id);
   }
 
   @Get(':id/messages')
   @UseGuards(IsValidChatRoom)
+  @ApiOperation({ summary: 'Regresa los mensajes filtrados por sala de chat y paginados.' })
   getMessages(@Param('id') id: string, @Query() options:PaginationOptions) {
     return this.chatRoomsService.getMessages(id, options);
   }
 
   @Post(':id/text-message')
   @UseGuards(IsValidChatRoom)
+  @ApiOperation({ summary: 'Guarda un mensaje de texto.' })
   textMessage(@Auth() { id }: UserPayload, @Param('id') chat_room_id: string, @Body() data: CreateTextMessageDto) {
     return this.chatRoomsService.textMessage(id, chat_room_id, data);
   }
@@ -61,6 +67,7 @@ export class ChatRoomsController {
   @Post(':id/file-message')
   @UseGuards(IsValidChatRoom)
   @FormDataRequest()
+  @ApiOperation({ summary: 'Guarda un mensaje con archivo.' })
   fileMessage(@Auth() { id }: UserPayload, @Param('id') chat_room_id: string, @Body() data: CreateFileMessageDto) {
     return this.chatRoomsService.fileMessage(id, chat_room_id, data);
   }
@@ -68,6 +75,7 @@ export class ChatRoomsController {
   @Get(':id/search-message')
   @UseGuards(IsValidChatRoom)
   @FormDataRequest()
+  @ApiOperation({ summary: 'Busca los mensajes filtrados por sala de chat.' })
   searchMessage( @Param('id') id: string, @Query() data:SearchMessageDto) {
     return this.chatRoomsService.searchMessage(id, data );
   }
--- src/modules/messages/messages.controller.ts
+++ src/modules/messages/messages.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Body, Param, Delete, UseGuards, Put } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { IsValidMessage } from 'src/common/helpers/guards/is-valid-message.guard';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('messages')
 @ApiBearerAuth()
@@ -12,18 +12,21 @@ export class MessagesController {
 
   @Get(':id')
   @UseGuards(IsValidMessage)
+  @ApiOperation({ summary: 'Regresa el detalle de un mensaje.' })
   findOne(@Param('id') id: string) {
     return this.messagesService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   @UseGuards(IsValidMessage)
+  @ApiOperation({ summary: 'Actualiza un mensaje.' })
   update(@Param('id') id: string, @Body() updateMessageDto: UpdateMessageDto) {
     return this.messagesService.update(id, updateMessageDto);
   }
 
   @Delete(':id')
   @UseGuards(IsValidMessage)
+  @ApiOperation({ summary: 'Elimina un mensaje.' })
   remove(@Param('id') id: string) {
     return this.messagesService.remove(id);
   }
